fe4a4c888d7853e514b89b4bed7f76b99346f216
Jimmy Cuadra <jimmy@jimmycuadra.com>
2022-02-01 00:02:13 -0800

    Add most-common subcommand.

--- src/cli.ts
+++ src/cli.ts
@@ -6,13 +6,11 @@ import glob from "glob";
 
 import { parsePaths } from "./";
 
-const main = async () => {
-  const rawRootPath = process.argv[process.argv.length - 1];
+const mostCommon = async (args: string[]) => {
+  const rawRootPath = args[0];
 
   if (rawRootPath == null) {
-    console.error(
-      "A directory containing JavaScript source files must be given."
-    );
+    console.error("Error: Missing required argument: DIR");
     process.exit(1);
   }
 
@@ -29,6 +27,11 @@ const main = async () => {
       process.exit(1);
     }
 
+    if (paths.length === 0) {
+      console.error(`Error: No JS or JSX source files found under path "${rootPath}".`);
+      process.exit(1);
+    }
+
     const importCountMap = await parsePaths(
       resolve(rootPath),
       paths.map((path) => resolve(path))
@@ -38,4 +41,27 @@ const main = async () => {
   });
 };
 
-main();
+const help = () => {
+  console.log(`
+import-count
+
+Usage: import-count COMMAND [...OPTIONS]
+
+Commands:
+  most-common DIR - Print every import found in the JS and JSX files in DIR,
+                    sorted with the most frequently occurring first.
+`);
+};
+
+export const run = async () => {
+  const args = process.argv.slice(2);
+
+  if (args[0] === "most-common") {
+    mostCommon(args.slice(1));
+  } else if (args[0] === "help" || args[0] === "--help") {
+    help();
+  } else {
+    console.log("Error: A command is required! Run `import-count help` to list commands.");
+    process.exit(1);
+  }
+};
