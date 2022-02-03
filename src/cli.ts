defe3e5e545a12c7cd27bceb7987c53360754725
Jimmy Cuadra <jimmy@jimmycuadra.com>
2022-02-02 23:50:45 -0800

    Use more succinct help messages.

--- src/cli.ts
+++ src/cli.ts
@@ -50,27 +50,21 @@ const mostCommon = async (rawRootPath: string, options: { json: boolean }) => {
   );
 };
 
-export const run = async () => {
-  const command = new Command("import-count").version("0.1.1");
+export const run = async (version: string) => {
+  const command = new Command("import-count")
+    .version(version)
+    .showHelpAfterError(true)
+    .showSuggestionAfterError(true);
 
   command
     .command("most-common")
     .argument(
       "<dir>",
-      `Absolute or relative path to a directory containing
-            JS and/or JSX source files.`
+      "absolute or relative path to a directory containing JS and/or JSX source files"
     )
-    .option("--json", "Output JSON instead of human-readable text.")
+    .option("--json", "output JSON instead of human-readable text")
     .description(
-      `Print every import found in the JS and JSX files in <dir>, sorted with the
-most frequently occurring first. By default, the output is human-readable,
-with each line showing the import as it would appear in JavaScript code,
-followed by the number of times that identifier was imported from that
-module. If the --json flag is given, the output will be JSON, where the keys
-are module names and the values are an array of objects representing each
-identifier imported from that module, including the kind of import (named,
-default, or namespace), and the number of times that identifier was imported
-from that module.`
+      "print the number of occurrences of each unique import found in the files within <dir>"
     )
     .action(mostCommon);
 
